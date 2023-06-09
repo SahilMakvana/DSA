a9e206faad922c93cc25037708573136cdad5e03
Sahil Makvana <17bce053@nirmauni.ac.in>
2023-06-09 17:18:44 +0530

    Commit

--- CPP_PROGRAMS/10__55__Intersaction_of_array.cpp
+++ CPP_PROGRAMS/10__55__Intersaction_of_array.cpp
@@ -5,7 +5,7 @@
 #include <limits.h>
 using namespace std;
 
-int findDuplicate(int arr1[], int size1, int arr2[], int size2)
+int findDuplicateSol1(int arr1[], int size1, int arr2[], int size2)
 {
     for (int i = 0; i < size1; i++)
     {
@@ -19,6 +19,30 @@ int findDuplicate(int arr1[], int size1, int arr2[], int size2)
             }
         }
     }
+    cout << endl;
+    return 0;
+}
+
+int findDuplicateSol2(int arr1[], int size1, int arr2[], int size2)
+{
+    int i = 0, j = 0;
+    while ((i < size1) && (j < size2))
+    {
+        if (arr1[i] < arr2[j])
+        {
+            i++;
+        }
+        else if (arr1[i] == arr2[j])
+        {
+            cout << arr1[i] << " ";
+            i++;
+            j++;
+        }
+        else
+        {
+            j++;
+        }
+    }
 
     return 0;
 }
@@ -28,7 +52,8 @@ int main()
     int arr1[6] = {1, 2, 2, 2, 3, 4};
     int arr2[4] = {2, 2, 3, 3};
 
-    findDuplicate(arr1, 6, arr2, 4);
+    findDuplicateSol1(arr1, 6, arr2, 4);
+    findDuplicateSol2(arr1, 6, arr2, 4);
 
     return 0;
 }
