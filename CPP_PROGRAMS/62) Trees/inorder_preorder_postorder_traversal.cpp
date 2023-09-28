35086daa7c42c2827c3d1fa46b2f49bad54b56ba
Sahil Makvana <17bce053@nirmauni.ac.in>
2023-09-28 23:04:22 +0530

    Commit

--- CPP_PROGRAMS/62) Trees/inorder_preorder_postorder_traversal.cpp
+++ CPP_PROGRAMS/62) Trees/inorder_preorder_postorder_traversal.cpp
@@ -40,14 +40,38 @@ node *buildTree(node *root)
 
 void inorder(node *root)
 {
+    if (root == NULL)
+    {
+        return;
+    }
+
+    inorder(root->left);
+    cout << root->data << " ";
+    inorder(root->right);
 }
 
 void preorder(node *root)
 {
+    if (root == NULL)
+    {
+        return;
+    }
+
+    cout << root->data << " ";
+    preorder(root->left);
+    preorder(root->right);
 }
 
 void postorder(node *root)
 {
+    if (root == NULL)
+    {
+        return;
+    }
+
+    postorder(root->left);
+    postorder(root->right);
+    cout << root->data << " ";
 }
 
 int main()
@@ -55,6 +79,7 @@ int main()
     // 1 3 7 -1 -1 11 -1 -1 5 17 -1 -1 -1
     node *root = NULL;
     cout << "Enter the Root: " << endl;
+    root = buildTree(root);
     inorder(root);
     preorder(root);
     postorder(root);
